d9154ac09bdbcd6575d10f244ca84dad6bec0ed5
William Morgensol <201606569@post.au.dk>
2019-05-26 21:33:52 +0200

    Cleaned up main from sender side

--- InfraredDataTransfer/src/main.cpp
+++ InfraredDataTransfer/src/main.cpp
@@ -16,21 +16,20 @@ void setup() {
 }
 
 void loop() {
-  //Data setup
-  
-  #ifdef SENDER
-  BaseTimer* a = new CTCTimer(1,38000);
-  IRTransmitter transmitter = IRTransmitter(new ManchesterEncoder(),a);
-  TempSensor temp =TempSensor();
-  char data[1024];
-  uint32_t length=0;
-  #endif
+  // Setup of classes
+    #ifdef SENDER
+    BaseTimer* a = new CTCTimer(1,38000);
+    IRTransmitter transmitter = IRTransmitter(new ManchesterEncoder(),a);
+    TempSensor temp =TempSensor();
+    char data[1024];
+    uint32_t length=0;
+    #endif
 
-  #ifdef RECIEVER
-  IRReceiver reciever = IRReceiver(0, true);
-  ITDB02 display= ITDB02();
-  #endif
-  // Loop to run infinetly
+    #ifdef RECIEVER
+    IRReceiver reciever = IRReceiver(true);
+    ITDB02 display= ITDB02();
+    #endif
+    //Loop to run infinetly
   while(1){
     #ifdef SENDER
     Serial.write("Enter Message and press enter to send: \n\r");
@@ -40,18 +39,22 @@ void loop() {
     while(length==0);
     if(data[0]=='T' && data[1]=='E' && data[2]=='M' && data[3]=='P')
     {
+      Serial.write("TEMP");
       snprintf(data,20,"%s",temp.getTemp());
-      length=20;
+      transmitter.sendData((uint8_t*)data,20);
     }
     else if(data[0]=='H' && data[1]=='U' && data[2]=='M')
     {
+      Serial.write("HUM");
       snprintf(data,20,"%s",temp.getHum());
-      length=20;
+      transmitter.sendData((uint8_t*)data,20);
     }
+    else{
       Serial.write("Sending...\n\r");
       transmitter.sendData((uint8_t*)data,length);
       Serial.write("Data send!\n\r");
       msleep(150+length);
+    }
     #endif
 
     #ifdef RECIEVER
@@ -60,6 +63,5 @@ void loop() {
     display.drawString(ReadData.streng,ReadData.length);
     delete[] ReadData.streng;
     #endif
-  }
-  
+  }  
 }
